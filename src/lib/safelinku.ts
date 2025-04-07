820113e9b207293c7dc1cac9525ba825dd185911
Aldinal Code <ronal.aldinal21@gmail.com>
2025-04-07 20:48:41 +0700

    safelinku fix

--- src/lib/safelinku.ts
+++ src/lib/safelinku.ts
@@ -7,6 +7,9 @@ export async function safelinku(
   const headers = {
     Authorization: `Bearer ${process.env.SAFELINKU_TOKEN}`, // Ganti dengan token Anda
     "Content-Type": "application/json",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    Accept: "application/json",
   };
   const body = {
     url: urlDownload,
