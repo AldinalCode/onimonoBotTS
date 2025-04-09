32f11e6029409e40ca538a28811907f6f13f0278
Aldinal Code <ronal.aldinal21@gmail.com>
2025-04-09 12:53:57 +0700

    fix safelinku

--- src/lib/safelinku.ts
+++ src/lib/safelinku.ts
@@ -1,43 +1,45 @@
 import axios from "axios";
+import puppeteer from "puppeteer";
 
 export async function safelinku(
   urlDownload: string
 ): Promise<string | undefined> {
-  const url = "https://safelinku.com/api/v1/links";
-  const headers = {
-    Authorization: `Bearer ${process.env.SAFELINKU_TOKEN}`,
-    "Content-Type": "application/json",
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    Accept: "application/json",
-  };
-  const body = {
-    url: urlDownload,
-  };
-
-  console.log("URL:", url);
-  console.log("Headers:", headers);
-  console.log("Body:", body);
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
   try {
-    const response = await axios.post(url, body, { headers });
-    console.log("Response Status:", response.status);
-    console.log("Response Data:", response.data);
+    await page.goto("https://safelinku.com/api/v1/links", {
+      waitUntil: "networkidle2",
+    });
 
-    if (response.status === 201 && response.data && response.data.url) {
-      const data = response.data;
-      const shortLink = data.url;
-      return shortLink;
-    }
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      if (request.url() === "https://safelinku.com/api/v1/links") {
+        request.continue({
+          method: "POST",
+          postData: JSON.stringify({ url: urlDownload }),
+          headers: {
+            Authorization: `Bearer ${process.env.SAFELINKU_TOKEN}`,
+            "Content-Type": "application/json",
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+          },
+        });
+      } else {
+        request.continue();
+      }
+    });
+
+    const response = await page.waitForResponse(
+      "https://safelinku.com/api/v1/links"
+    );
+    const data = await response.json();
+
+    await browser.close();
+    return data.url;
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error(
-        "Axios Error:",
-        error.response?.status,
-        error.response?.data
-      );
-    } else {
-      console.error("Error fetching data:", error);
-    }
+    console.error("Error with Puppeteer:", error);
+    await browser.close();
+    return undefined;
   }
 }
