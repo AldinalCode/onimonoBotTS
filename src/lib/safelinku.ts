5c99680df31f4f58ea1b20b4f7cf1ab0f49c2391
Aldinal Code <ronal.aldinal21@gmail.com>
2025-04-09 12:46:00 +0700

    fix safelinku

--- src/lib/safelinku.ts
+++ src/lib/safelinku.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 export async function safelinku(
   urlDownload: string
 ): Promise<string | undefined> {
-  const url = "https://safelinku.com/api/v1/links"; // Ganti dengan URL API yang sesuai
+  const url = "https://safelinku.com/api/v1/links";
   const headers = {
-    Authorization: `Bearer ${process.env.SAFELINKU_TOKEN}`, // Ganti dengan token Anda
+    Authorization: `Bearer ${process.env.SAFELINKU_TOKEN}`,
     "Content-Type": "application/json",
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
@@ -15,16 +15,29 @@ export async function safelinku(
     url: urlDownload,
   };
 
+  console.log("URL:", url);
+  console.log("Headers:", headers);
+  console.log("Body:", body);
+
   try {
     const response = await axios.post(url, body, { headers });
-    // console.log("Response:", responsm
+    console.log("Response Status:", response.status);
+    console.log("Response Data:", response.data);
 
     if (response.status === 201 && response.data && response.data.url) {
       const data = response.data;
-      const shortLink = data.url; // Ganti dengan field yang sesuai dari respons API
+      const shortLink = data.url;
       return shortLink;
     }
   } catch (error) {
-    console.error("Error fetching data:", error);
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Axios Error:",
+        error.response?.status,
+        error.response?.data
+      );
+    } else {
+      console.error("Error fetching data:", error);
+    }
   }
 }
